06c460ee05484dc560ce19506006db0b94437d22
居戎氏 <chen.sst@gmail.com>
2022-09-16 21:26:42 +0800

    mod-tap Ctrl/A, Alt/Z

--- keymap.c
+++ keymap.c
@@ -52,6 +52,8 @@ enum custom_keycodes {
 #define AL_ESC LALT_T(KC_ESC)
 #define CM_BSPC LCMD_T(KC_BSPC)
 
+#define CT_A LCTL_T(KC_A)
+#define AL_Z LALT_T(KC_Z)
 #define CT_SCLN RCTL_T(KC_SCLN)
 #define AL_SLSH RALT_T(KC_SLSH)
 
@@ -67,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        ADJUST,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  ADJUST,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        RAISE,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, CT_SCLN, RA_QUOT,
+        RAISE,    CT_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, CT_SCLN, RA_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, AL_SLSH, KC_RSFT,
+      KC_LSFT,    AL_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, AL_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                            LO_TAB,  CT_ENT,  AL_ESC,    CM_BSPC,  KC_SPC,  LO_DEL
                                       //`--------------------------'  `--------------------------'
