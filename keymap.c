ce79a7bdb596325111bbfc814578ba8b071f8068
居戎氏 <chen.sst@gmail.com>
2022-11-30 15:00:28 +0800

    s/RESET/QK_BOOT/

--- keymap.c
+++ keymap.c
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [_ADJUST] = LAYOUT_split_3x6_3(
-        RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              XXXXXXX, KC_WH_U, KC_WH_D, XXXXXXX, XXXXXXX, KC_SLEP,
+        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              XXXXXXX, KC_WH_U, KC_WH_D, XXXXXXX, XXXXXXX, KC_SLEP,
         STN_ON,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                             KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
         _______, KC_F11,  KC_F12,  RGB_RMOD,RGB_MOD, RGB_TOG,                            XXXXXXX, KC_BTN3, KC_BTN4, KC_BTN5, XXXXXXX, _______,
                                             _______, _______, _______,          KC_BTN1, KC_BTN2, _______
