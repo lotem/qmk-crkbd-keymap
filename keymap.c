41498d53d97dbeefa8037b8e041a9eb15e7ef4c6
居戎氏 <chen.sst@gmail.com>
2022-12-16 12:00:10 +0800

    Revert "40% keymap with number row"
    This reverts commit 6a0ce62e5f7045a44dfaebc667367706d0fb569c.
    
    Rename layers: Alpha, Symbol, Number, Function, Steno

--- keymap.c
+++ keymap.c
@@ -21,9 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum Layers {
     _ALPHA,
-    _LOWER,
-    _RAISE,
-    _ADJUST,
+    _SYMBOL,
+    _NUMBER,
+    _FUNCTION,
     _STENO,
 };
 
@@ -37,9 +37,9 @@ enum custom_keycodes {
 #include "features/caps_word.h"
 #include "features/steno.h"
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-#define ADJUST MO(_ADJUST)
+#define SYM MO(_SYMBOL)
+#define NUM MO(_NUMBER)
+#define FN MO(_FUNCTION)
 
 #define OS_LCTL OSM(MOD_LCTL)
 #define OS_RCTL OSM(MOD_RCTL)
@@ -48,10 +48,10 @@ enum custom_keycodes {
 #define OS_LCMD OSM(MOD_LGUI)
 #define OS_RCMD OSM(MOD_RGUI)
 
-#define ADJ_ESC LT(_ADJUST, KC_ESC)
-#define ADJ_QUO LT(_ADJUST, KC_QUOT)
-
 #define CTL_ENT LCTL_T(KC_ENT)
+
+#define FN_ESC LT(_FUNCTION, KC_ESC)
+#define FN_QUOT LT(_FUNCTION, KC_QUOT)
 
 #define LAYOUT_split_3x6_3_wrapper(...) LAYOUT_split_3x6_3(__VA_ARGS__)
 
@@ -66,23 +66,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
       *               │Cmd├───┐Ctl        ┌───┤Alt│
-      *               └───┤LOW├───┐   ┌───┤RAI├───┘
+      *               └───┤SYM├───┐   ┌───┤NUM├───┘
       *                   └───┤Ent│   │   ├───┘
       *                       └───┘   └───┘
       */
     [_ALPHA] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        ADJ_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, ADJ_QUO,
+        FN_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, FN_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LCMD, LOWER,   CTL_ENT,          KC_SPC,  RAISE,   KC_RALT
+                                            KC_LCMD, SYM,     CTL_ENT,          KC_SPC,  NUM,     KC_RALT
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │ ~ │ ! │ @ │ # │ $ │ % │       │ ^ │ & │ * │ ( │ ) │Del│
+      * │   │   │ [ │ { │ } │   │       │ ^ │ ( │ ) │ ] │ ~ │ " │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Cap│ ` │ [ │ { │ } │ ] │       │ \ │ - │ = │ ; │ ' │   │
+      * │   │ ! │ @ │ # │ $ │ % │       │ & │ - │ = │ \ │ ` │ ' │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │ ~ │Mut│Vo-│Vo+│Ply│       │ | │ _ │ + │ : │ " │   │
+      * │   │   │   │   │   │   │       │ * │ _ │ + │ | │ / │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
       *               │Cmd├───┐           ┌───┤Alt│
@@ -90,20 +90,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┤Ctl│   │   ├───┘
       *                       └───┘   └───┘
       */
-    [_LOWER] = LAYOUT_split_3x6_3(
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-        KC_CAPS, KC_GRV,  KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC,                            KC_BSLS, KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, _______,
-        _______, KC_TILD, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY,                            KC_PIPE, KC_UNDS, KC_PLUS, KC_COLN, KC_DQUO, _______,
+    [_SYMBOL] = LAYOUT_split_3x6_3(
+        _______, XXXXXXX, KC_LBRC, KC_LCBR, KC_RCBR, XXXXXXX,                            KC_CIRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_TILD, KC_DQUO,
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_AMPR, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_QUOT,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_ASTR, KC_UNDS, KC_PLUS, KC_PIPE, KC_SLSH, _______,
                                             OS_LCMD, _______, OS_LCTL,          _______, _______, OS_LALT
 
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │       │ 6 │ 7 │ 8 │ 9 │ 0 │Del│
+      * │   │ + │ 1 │ 2 │ 3 │ : │       │Hom│PgD│PgU│End│ " │Del│
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │Cap│F1 │F2 │F3 │F4 │F5 │       │ L │ D │ U │ R │F11│F12│
+      * │   │ - │ 4 │ 5 │ 6 │ . │       │ L │ D │ U │ R │ ' │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │F6 │F7 │F8 │F9 │F10│       │Hom│PgD│PgU│End│PSc│   │
+      * │   │Cap│ 7 │ 8 │ 9 │ 0 │       │   │Mut│Vo-│Vo+│Ply│   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
       *               │Cmd├───┐           ┌───┤Alt│
@@ -111,19 +111,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┤Ctl│   │   ├───┘
       *                       └───┘   └───┘
       */
-    [_RAISE] = LAYOUT_split_3x6_3(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-        KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F11,  KC_F12,
-        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                             KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR, _______,
+    [_NUMBER] = LAYOUT_split_3x6_3(
+        _______, KC_PLUS, KC_1,    KC_2,    KC_3,    KC_COLN,                            KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DQUO, KC_DEL,
+        _______, KC_MINS, KC_4,    KC_5,    KC_6,    KC_DOT,                             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______,
+        _______, KC_CAPS, KC_7,    KC_8,    KC_9,    KC_0,                               XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______,
                                             OS_RCMD, _______, OS_RCTL,          _______, _______, OS_RALT
     ),
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │RST│F1 │F2 │F3 │F4 │F5 │       │   │WhU│WhD│   │   │SLP│
+      * │RST│   │F1 │F2 │F3 │F10│       │   │WhU│WhD│   │PSc│SLP│
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │STN│F6 │F7 │F8 │F9 │F10│       │MsL│MsD│MsU│MsR│   │   │
+      * │STN│   │F4 │F5 │F6 │F11│       │MsL│MsD│MsU│MsR│BL+│BL/│
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │F11│F12│BL-│BL+│BL/│       │   │MB3│MB4│MB5│   │   │
+      * │   │   │F7 │F8 │F9 │F12│       │   │MB3│MB4│MB5│   │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
       *               │   ├───┐           ┌───┤   │
@@ -131,10 +131,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┤   │   │MB1├───┘
       *                       └───┘   └───┘
       */
-    [_ADJUST] = LAYOUT_split_3x6_3(
-        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              XXXXXXX, KC_WH_U, KC_WH_D, XXXXXXX, XXXXXXX, KC_SLEP,
-        STN_ON,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                             KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
-        _______, KC_F11,  KC_F12,  RGB_RMOD,RGB_MOD, RGB_TOG,                            XXXXXXX, KC_BTN3, KC_BTN4, KC_BTN5, XXXXXXX, _______,
+    [_FUNCTION] = LAYOUT_split_3x6_3(
+        QK_BOOT, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F10,                             XXXXXXX, KC_WH_U, KC_WH_D, XXXXXXX, KC_PSCR, KC_SLEP,
+        STN_ON,  XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,                             KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, RGB_MOD, RGB_TOG,
+        _______, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F12,                             XXXXXXX, KC_BTN3, KC_BTN4, KC_BTN5, XXXXXXX, _______,
                                             _______, _______, _______,          KC_BTN1, KC_BTN2, _______
     ),
 
